898c500f2ff80fb269eae23f2848cdfe3f1fdf3b
laurindolucas <caiolucax2024@gmail.com>
2026-04-07 09:39:31 -0300

    feat: adicionando estrutura para o corpo da requisição em Json e testando via insomnia

--- curso-node/aulas/01-fundamentos-nodejs/src/server.js
+++ curso-node/aulas/01-fundamentos-nodejs/src/server.js
@@ -3,24 +3,36 @@ import http from 'node:http'
 
 
 const users = []
-const server = http.createServer((req, res) =>{
+const server = http.createServer(async(req, res) => {
 
-    const {method, url}  = req
+    const { method, url } = req
+    const buffers = []
 
-    if (method == 'GET' && url =='/users'){
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+    try{
+         req.body = JSON.parse(Buffer.concat(buffers).toString())
+
+    }catch{
+        req.body = null
+    }
+
+    if (method == 'GET' && url == '/users') {
         return res
-        .setHeader('Content-type','application/json')
-        .end(JSON.stringify(users))
-    } 
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
 
-    if (method == 'POST' && url == '/users'){
+    if (method == 'POST' && url == '/users') {
+        const {name, email} = req.body
         users.push({
             id: 1,
-            name : 'caio',
-            email: 'caiolaurindodev@gmail.com'
-    }) 
-        
-        
+            name,
+            email,
+        })
+
+
         return res.writeHead(201).end()
     }
 
