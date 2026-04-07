c1117ce9e3fc772f4b9a109523edd8f71fb91609
laurindolucas <caiolucax2024@gmail.com>
2026-04-07 09:58:41 -0300

    feat: adicioanndo uma rapida explicação sobre buffers

---
+++ curso-node/aulas/02-streams/buffer.js
@@ -0,0 +1,7 @@
+// buffer é basicamente um espeaço na memoria do 
+// seu compuatador usado para transitar dados de uma 
+// maneira muito rapida, ele armazena os valores em binario e geralmente no hexademicial
+
+const buf = Buffer.from('mirella')
+
+console.log(buf)
