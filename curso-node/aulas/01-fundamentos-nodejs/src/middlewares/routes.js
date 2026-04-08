af7c602f50e9579e26bddcf9e13742fc24b96395
laurindolucas <caiolucax2024@gmail.com>
2026-04-08 11:54:34 -0300

    feat:a adicionando logica e rota de delete, usando regex tambem

--- curso-node/aulas/01-fundamentos-nodejs/src/middlewares/routes.js
+++ curso-node/aulas/01-fundamentos-nodejs/src/middlewares/routes.js
@@ -1,5 +1,6 @@
 import { Database } from "../database.js"
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "../utils/build-route-path.js"
 
 const database = new Database()
 
@@ -7,7 +8,7 @@ const database = new Database()
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users')
             return res.end(JSON.stringify(users))
@@ -15,7 +16,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body
             const user = {
@@ -29,5 +30,15 @@ export const routes = [
 
             return res.writeHead(201).end()
         }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler:(req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+            return res.writeHead(204).end()
+        },
     }
 ]
