7d2be57c92bd30c2b1ec459c2c36028317639381
laurindolucas <caiolucax2024@gmail.com>
2026-04-09 09:56:39 -0300

    feat: adicionando função de atualizar

--- curso-node/aulas/01-fundamentos-nodejs/src/middlewares/routes.js
+++ curso-node/aulas/01-fundamentos-nodejs/src/middlewares/routes.js
@@ -34,11 +34,37 @@ export const routes = [
     {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
-        handler:(req, res) => {
+        handler: (req, res) => {
             const { id } = req.params
 
             database.delete('users', id)
             return res.writeHead(204).end()
         },
+
+    }, 
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const {name, email} = req.body
+
+            database.update('users', id,{
+                name,
+                email
+            })
+            return res.writeHead(204).end()
+        },
+
+    }, {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+            return res.writeHead(204).end()
+        },
+
     }
 ]
